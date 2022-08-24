6a0369e70f1f574d8f198e52d2bedda382373fd8
Iryna Vakilova <iravakilova@gmail.com>
2022-08-24 15:54:26 +0300

    Update Statistics.js

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -3,18 +3,24 @@ import { Box } from 'components/Box';
 import { Title, StatList, StatItem } from './StyledStatistics';
 
 export const Statistics = ({ title, stats }) => {
-    return (<Box mx = "auto" py = {4}>
-  <Title>{title}</Title>
-        <StatList>
-            {stats.map(({ id, label, percentage }) =>
-            (<StatItem key={id}>
-                <Box fontSize = "s" color = "white">{label}</Box>
-                <Box fontSize = "m" color = "white" fontWeight = "bold">{percentage}%</Box>
-            </StatItem>))}
-        </StatList>   
+  return (
+    <Box mx="auto" py={4}>
+      {title && <Title>{title}</Title>}
+      <StatList>
+        {stats.map(({ id, label, percentage }) => (
+          <StatItem key={id}>
+            <Box fontSize="s" color="white">
+              {label}
+            </Box>
+            <Box fontSize="m" color="white" fontWeight="bold">
+              {percentage}%
+            </Box>
+          </StatItem>
+        ))}
+      </StatList>
     </Box>
-    )
-}
+  );
+};
 
 Statistics.propTypes = {
   title: PropTypes.string,
